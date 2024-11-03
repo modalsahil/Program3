e72248b013a2371a445338a61838f85d3fa2e9b6
modalsahil <sahilsahota1313@gmail.com>
2024-11-02 21:09:32 -0700

    1

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,33 +1,16 @@
 #include "linked_list.h"
 
-using namespace std;
-
 int main() {
-}
 
-template <class T>
-List342<T>::List342() : head_(nullptr), tail_(nullptr) {
-}
+    List342<int> test;
 
-template <class T>
-bool List342<T>::Insert(T *obj) {
-    Node insert_node = new Node();
-    insert_node.value = obj;
+    int a = 4;
+    int b = 2;
+    int c = 7;
 
-    Node next_node = head_;
+    test.Insert(&a);
+    test.Insert(&b);
+    test.Insert(&c);
 
-    for (int i = 0; i < size(); i++) {
-        if (next_node.value > insert_node.value) {
-            next_node.link = insert_node;
-            insert_node.link = next_
-        } else {
-            next_node = next_node.link;
-        }
-    }
-    size++; 
-}
-
-template <class T>
-int List342<T>::size() const {
-    return size;
+    cout << test << endl; 
 }
--- linked_list.h
+++ linked_list.h
@@ -1,73 +1,128 @@
 #ifndef LINKED_LIST_H_
 #define LINKED_LIST_H_
-#include <string>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-template<class T>
-struct Node{
-    Node* link = nullptr;
-    T value; = 0; 
-    
+template <class T>
+struct Node {
+    Node *link = nullptr;
+    T value = 0;
 };
 
 template <class T>
-class List342{
+class List342 {
 
-    public: 
-    
-    //constructors
+public:
+    // constructors
     List342();
 
-    //deconstructor
+    // deconstructor
     ~List342();
 
-    //takes input from a file and builds list
+    // takes input from a file and builds list
     bool BuildList(string file_name);
 
-    bool Insert(T* obj);
+    bool Insert(T *obj);
 
-    bool Remove(T target, T& result);
+    bool Remove(T target, T &result);
 
-    //finds and returns a T object
-    bool Peek(T target, T& result) const; 
+    // finds and returns a T object
+    bool Peek(T target, T &result) const;
 
-    //getters and setters
-    int size() const; 
+    // getters and setters
+    int size() const;
 
-//deletes list and deallocates all memmory, 
-    void DeleteList(); //probably uses Remove()
+    Node<T> &head() const;
 
-    //merges 2 lists, deletes list1
-    bool Merge(List342& list1); //definetely uses DeleteList()
+    // deletes list and deallocates all memmory,
+    void DeleteList(); // probably uses Remove()
 
-    //operator overloads
-    List342& operator+=(const List342 &other); 
+    // merges 2 lists, deletes list1
+    bool Merge(List342 &list1); // definetely uses DeleteList()
+
+    // operator overloads
+    List342 &operator+=(const List342 &other);
 
     List342 operator+(const List342 &other) const;
 
-    friend ostream& operator<<(ostream& stream, const List342& print_list) const; 
+    template <typename U>
+    friend ostream &operator<<(ostream &stream, const List342<U> &print_list);
 
-    bool operator==(const List342 &other) const; 
+    bool operator==(const List342 &other) const;
 
-    List342& operator=(list342 &other); // needs to dcall DeleteList(); 
+    List342 &operator=(List342 &other); // needs to dcall DeleteList();
 
+private:
+    // private method to Sort list
+    // bool Sort(Node* insert_node);
 
-    private:
+    // checks to see if a T objct is in the list.
+    bool Contains(const T *obj) const;
 
-    //private method to Sort list
-    //bool Sort(Node* insert_node); 
-
-    //checks to see if a T objct is in the list. 
-    bool Contains(const T* obj) const; 
-
-    Node head_; 
-    Node tail_; 
-    unsigned int size_; 
-
-
-
+    Node<T> *head_;
+    unsigned int size_;
 };
-#endif
 
+template <class T>
+List342<T>::List342() : head_(nullptr) {
+}
+
+template <class T>
+List342<T>::~List342() {
+}
+
+template <class T>
+bool List342<T>::Insert(T *obj) {
+    cout << "here 1" << endl;
+    Node<T> *insert_node = new Node<T>;
+    insert_node->value = *obj;
+
+    Node<T> *next_node = new Node<T>;
+    next_node = head_;
+    if (head_ == nullptr) {
+        head_ = insert_node;
+        return true; 
+    }
+    for (int i = 0; i < size(); i++) {
+        if (next_node->link->value > insert_node->value) {
+            insert_node->link = next_node->link;
+            next_node->link = insert_node;
+            size_++;
+            return true;
+        } else if (next_node->value == insert_node->value) {
+            insert_node = nullptr;
+            delete insert_node;
+            return false;
+        }
+    }
+    cout << "Failed to insert " << *obj << endl;
+    return false;
+}
+
+template <class T>
+int List342<T>::size() const {
+    return size_;
+}
+
+template <class T>
+Node<T> &List342<T>::head() const {
+    return *head_;
+}
+
+template <class T>
+ostream &operator<<(ostream &stream, const List342<T> &print_list) {
+    Node<T> *next_node = new Node<T>;
+    next_node = print_list.head_;
+
+    for (int i = 0; i < print_list.size(); i++) {
+        stream << next_node->value;
+        next_node = next_node->link;
+    }
+    next_node = nullptr;
+    delete next_node;
+    return stream;
+}
+
+#endif
