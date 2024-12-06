28846474ecee6c2d1c9ea8cc8bd2268b6f752e2d
modalsahil <sahilsahota1313@gmail.com>
2024-12-06 11:22:47 -0800

    1

--- test.cpp
+++ test.cpp
@@ -1,17 +1,31 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
-
-int calculateWays(int wordLen, int maxVowels){
-string current = "";
-
-for (int i = 0; i < wordLen; i++){
-    current +=  
-}
-
+template <typename T>
+int removeItem(T arr[], int length, T item) {
+    int newLength = 0;
+    for (int i = 0; i < length; ++i) {
+        if (arr[i] != item) {
+            arr[newLength++] = arr[i];
+        }
+    }
+    return newLength;
 }
 
 
 int main() {
 
+    int arr[4];
+
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 2;
+    arr[3] = 4;
+
+    int x = removeItem(arr, 4, 2);
+
+    for (int i = 0; i < x; i++){
+        cout << arr[i] << endl; 
+    }
 }
